ad447653a3636a6e8ee23c81b7e7c6bfe9555990
今日钓点 <dengzilin@schipera.com>
2025-05-21 17:29:09 +0800

    Create data.js

---
+++ data.js
@@ -0,0 +1,17 @@
+const fishingSpots = [
+  {
+    name: "龙岗水库",
+    location: "深圳市龙岗区",
+    link: "https://longgang.jinridiaodian.cn"
+  },
+  {
+    name: "电子河钓点",
+    location: "惠州市仲恺高新区",
+    link: "https://dianzihe.jinridiaodian.cn"
+  },
+  {
+    name: "松山湖钓点",
+    location: "东莞市松山湖科技园",
+    link: "https://songshanhu.jinridiaodian.cn"
+  }
+];
